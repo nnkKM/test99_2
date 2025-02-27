97d62fe802f4deb42398c53b88ce8955c32f2c19
nnkKM <kumi.miyata@nnk.co.jp>
2025-02-27 14:49:44 +0900

    Update main.js

--- main.js
+++ main.js
@@ -12,23 +12,24 @@ const map = new maplibregl.Map({
 });
 map.addControl(new maplibregl.NavigationControl());
 
-// 最初に2020年をセット
+// デフォルトの年数をセット
 map.on('load', () => {
     updateMapStyle_pop("2020");
-    updateMapStyle_pgr("2020");
-    updateMapStyle_pgr2("2020");
+    updateMapStyle_pgr("2019");
 });
 
 
 /*******************************************************************
  * レイヤON/OFF
  * *************************************************************** */
-
+// 人口データ以外のレイヤ設定はjsonのレイヤIDをここに加える
 const layerIds = [
     'osm-layer',
     'PublicTransport-points-layer'
 ];
 
+
+// 人口データのレイヤはここで切替
 document.querySelector('#population-all-fill-layer-chk').addEventListener('change', () => {
     const isChecked = document.getElementById('population-all-fill-layer-chk').checked;
     const fillLayerIds = ['population-fill-layer', 'population-outline-layer'];
@@ -52,19 +53,6 @@ document.querySelector('#pgr-all-fill-layer-chk').addEventListener('change', () 
         }
     });
 });
-
-document.querySelector('#pgr2-all-fill-layer-chk').addEventListener('change', () => {
-    const isChecked = document.getElementById('pgr2-all-fill-layer-chk').checked;
-    const fillLayerIds = ['pgr2-fill-layer', 'pgr2-outline-layer'];
-    fillLayerIds.forEach(id => {
-        if (isChecked) {
-            map.setLayoutProperty(id, 'visibility', 'visible');
-        } else {
-            map.setLayoutProperty(id, 'visibility', 'none');
-        }
-    });
-});
-
 
 document.querySelector('#toggle-layers-btn').addEventListener('click', () => {
     const layersContainer = document.getElementById('layers-container');
@@ -94,53 +82,38 @@ layerIds.forEach(lyrId => {
  * 年度ごとの表示切替
  * *************************************************************** */
 
-
+/////////////////   スライダーバーの設定　　//////////////////
 // 現在の年を保存する変数
 let currentYear = 2001;
 
 // スライドバーの要素を取得
-const yearSlider = document.getElementById('year-slider');
-const yearValue = document.getElementById('year-value');
+const yearSliderPop = document.getElementById('year-slider-pop');
+const yearValuePop = document.getElementById('year-value-pop');
+const yearSliderPgr = document.getElementById('year-slider-pgr');
+const yearValuePgr = document.getElementById('year-value-pgr');
 
-// スライドバーが変更されたときのイベントリスナーを追加
-yearSlider.addEventListener('input', (event) => {
+
+// スライドバーが変更されたときのイベント
+yearSliderPop.addEventListener('input', (event) => {
     const selectedYear = event.target.value;
-    yearValue.textContent = selectedYear;
-    updateMapStyle_pop(selectedYear);
-    updateMapStyle_pgr(selectedYear);
-    updateMapStyle_pgr2(selectedYear);
+    yearValuePop.textContent = selectedYear;    // 今何年選んでいるかの表示
+    updateMapStyle_pop(selectedYear);           // 色の設定の関数を実行
 });
 
+yearSliderPgr.addEventListener('input', (event) => {
+    const selectedYear = event.target.value;
+    yearValuePgr.textContent = selectedYear;
+    updateMapStyle_pgr(selectedYear);
+});
+
+
+
+/////////////////   色の設定　　//////////////////
 // 数字 = aSmallpop * (n ^ k) (k=1, 2, 3...)
 var aSmallpop = [16801, 2401, 343, 49, 7];  // 最初の数
 var n = 2;                                  // 何をかけるか
-// // 色を生成する関数
-// function generateColors(base, multiplier, steps, year) {
-//     let colors = [];
-//     for (let i = 0; i < steps; i++) {
-//         let value = base * Math.pow(multiplier, i);
-//         let color = `rgb(${255 - (i * 28)}, ${255 - (i * 28)}, ${255 - (i * 28)})`;
-//         colors.push([["<=", ["get", year], value], color]);
-//     }
-//     colors.push("rgb(255, 0, 0)");  // 最後の色
-//     return colors;
-// }
 
-// // スタイルを動的に更新する関数
-// function updateMapStyle_pop(year) {
-//     if (map.getLayer('population-fill-layer')) {
-//         let expressions = [3, 4.1, 8, 10, 12].map((zoom, index) => {
-//             return [zoom, ["case", ...generateColors(aSmallpop[index], n, 9, year).flat()]];
-//         });
-
-//         map.setPaintProperty('population-fill-layer', 'fill-color', [
-//             "step",
-//             ["zoom"],
-//             ...expressions.flat()
-//         ]);
-//     }
-// }
-// スタイルを動的に更新する関数
+// 色を設定する関数、yearの値によって色が変わる
 function updateMapStyle_pop(year) {
     if (map.getLayer('population-fill-layer')) {
         map.setPaintProperty('population-fill-layer', 'fill-color', [
@@ -215,66 +188,68 @@ function updateMapStyle_pop(year) {
     }
 }
 
+// 色を設定する関数、yearの値によって色が変わる
 function updateMapStyle_pgr(year) {
     if (map.getLayer('pgr-fill-layer')) {
-        map.setPaintProperty('pgr-fill-layer', 'fill-color', color_pgr(year));
-    }
-}
-function updateMapStyle_pgr2(year) {
-    if (map.getLayer('pgr2-fill-layer')) {
-        map.setPaintProperty('pgr2-fill-layer', 'fill-color', color_pgr2(year));
+        map.setPaintProperty('pgr-fill-layer', 'fill-color', color_pgr(year));  // 具体的な色の指定はcolor_pgrでしている
     }
 }
 
-
-// 参考：藤村さん作成色作成関数
-// const opacity = (y) => {
-//   return ["min", 1.0, ["/", ["log10", ["+", 1, ["get", String(y)]]], 5.0]]
-// }
+// 色の指定をしている、上の関数でyにはyearの値を入れる
 const color_pgr = (y) => {
-  return ["case",
-    ["any", ["<", ["get", String(y)], 500], ["<", ["get", String(y - 1)], 500]],
-    "#888888",
-    [
-      "interpolate-hcl",
-      ["linear"],
-      ["-", ["ln", ["get", String(y)]], ["ln", ["get", String(y - 1)]]],
-      -0.2, "rgb(39,42,149)",
-      -0.15, "rgb(39,42,197)",
-      -0.1, "rgb(39,42,246)",
-      -0.05,"rgb(141,144,249)",
-      0, "rgb(243,246,255)",
-      0.05, "rgb(243,246,117)",
-      0.1, "rgb(230,151,92)",
-      0.15, "rgb(226,83,79)",
-      0.2, "rgb(226,83,153)",
-      0.25, "rgb(226,83,249)"
-    ]
-  ]
-}
 
-const color_pgr2 = (y) => {
+    // zoomレベルの値を取得
+    const zoom = map.getZoom().toFixed(2);
+
+    // 色のパターンを設定
     return ["case",
-      ["any", ["<", ["get", String(2020)], 500], ["<", ["get", String(y)], 500]],
+    　// 人口が1未満の時は灰色（消すとエラー出る）
+      ["any", ["<", ["get", String(2020)], 1], ["<", ["get", String(y)], 1]],
       "#888888",
       [
-        "interpolate-hcl",
-        ["linear"],
-        ["-", ["ln", ["get", String(2020)]], ["ln", ["get", String(y)]]],
-        -0.2, "rgb(39,42,149)",
-        -0.15, "rgb(39,42,197)",
-        -0.1, "rgb(39,42,246)",
-        -0.05,"rgb(141,144,249)",
+        "interpolate-hcl",  // グラデーションを自動で付けるものらしい
+        ["linear"],  // グラデーションを自動で付けるものらしい
+        ["-", ["get", String(2020)], ["get", String(y)]], // 2020年との人口の値の差   
+        -1000000 / zoom, "rgb(0, 0, 255)",  // zoomの値にかけたり足したりして良い感じの数字に調整できる
+        -100000 / zoom, "rgba(0, 8, 255, 0.89)",
+        -10000 / zoom, "rgba(0, 8, 255, 0.68)",
+        -1000 / zoom,"rgba(0, 8, 255, 0.47)",
         0, "rgb(243,246,255)",
-        0.05, "rgb(243,246,117)",
-        0.1, "rgb(230,151,92)",
-        0.15, "rgb(226,83,79)",
-        0.2, "rgb(226,83,153)",
-        0.25, "rgb(226,83,249)"
+        1000 / zoom, "rgba(255, 0, 0, 0.25)",
+        10000 / zoom, "rgba(255, 0, 0, 0.43)",
+        100000 / zoom, "rgba(255, 5, 5, 0.59)",
+        1000000 / zoom, "rgba(255, 0, 0, 0.8)",
+        10000000 / zoom, "rgb(255, 0, 0)"
       ]
     ]
   }
   
+// 参考：藤村さん作成色作成関数
+// const opacity = (y) => {
+//   return ["min", 1.0, ["/", ["log10", ["+", 1, ["get", String(y)]]], 5.0]]
+// }
+// const color_pgr = (y) => {
+//   return ["case",
+//     ["any", ["<", ["get", String(y)], 500], ["<", ["get", String(y - 1)], 500]],
+//     "#888888",
+//     [
+//       "interpolate-hcl",
+//       ["linear"],
+//       ["-", ["ln", ["get", String(y)]], ["ln", ["get", String(y - 1)]]],
+//       -0.2, "rgb(39,42,149)",
+//       -0.15, "rgb(39,42,197)",
+//       -0.1, "rgb(39,42,246)",
+//       -0.05,"rgb(141,144,249)",
+//       0, "rgb(243,246,255)",
+//       0.05, "rgb(243,246,117)",
+//       0.1, "rgb(230,151,92)",
+//       0.15, "rgb(226,83,79)",
+//       0.2, "rgb(226,83,153)",
+//       0.25, "rgb(226,83,249)"
+//     ]
+//   ]
+// }
+
 
 
 /*******************************************************************
